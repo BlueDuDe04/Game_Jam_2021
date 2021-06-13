4c0830cbf20cdddbf7341f91e06179dfca576570
BlueDuDe <Bennettmason04@gmail.com>
2021-06-13 00:59:54 -0700

    Revert "MaybE"
    This reverts commit e7d94b6175f9494f42391842fa1e661102e151b8, reversing
    changes made to a541d7b74106ca8fa8f5b93655ad6d644e128b34.

--- Collaborative_PuzzleGame/Assets/Scripts/Core/Pickups/Keycard.cs
+++ Collaborative_PuzzleGame/Assets/Scripts/Core/Pickups/Keycard.cs
@@ -4,97 +4,70 @@ using UnityEngine;
 
 public class Keycard : MonoBehaviour, IPickup
 {
+    public int scannerID;
     Rigidbody rb;
-    BoxCollider collider;
     bool isHeld;
-    public Transform mirroredObject; 
-    public Transform MirrorPoint;
-    public float offsetZ;
-    public float offsetX;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        collider = GetComponent<BoxCollider>();
 
     }
-    
-    private void LateUpdate() 
-    {
-        
-        if (mirroredObject != null
-        && isHeld)
-        {
-            
-            //mirroredObject.position = Vector3.LerpUnclamped(this.transform.parent.position, MirrorPoint.position, 3f);
-            
-            float posX = Mathf.LerpUnclamped(transform.parent.position.x, MirrorPoint.position.z, offsetX);;
-            float posY = transform.parent.position.y;
-            float posZ = Mathf.LerpUnclamped(transform.parent.position.z, MirrorPoint.position.z, offsetZ);
-
-            mirroredObject.transform.position = new Vector3(posX, posY, posZ);
-            //Transform playerPos = this.transform.parent.parent.parent.parent.transform;
-            //mirroredObject.transform.position = playerPos.position;
-
-        }
-    }
-
 
     public void OnDrop()
     {
-        rb.ResetCenterOfMass();
-        rb.velocity = Vector3.zero;
-        rb.AddForce(Vector3.up * 700);
-
         isHeld = false;
-        this.transform.parent = null;
-
-        collider.isTrigger = false;
-
-        //GetComponent<Collider>().enabled = true;
-        rb.constraints = RigidbodyConstraints.None;
-        this.transform.parent = null;
-
-        if (mirroredObject != null)
-            mirroredObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
 
         
+        rb.constraints = RigidbodyConstraints.None;
+        this.transform.parent = null;
+        GetComponent<Collider>().isTrigger = false;
     }
 
     public void OnPickup()
     {
         isHeld = true;
         Debug.Log("Pick up this " + this.gameObject.name);
-        GetComponent<Collider>().isTrigger = true;
-        //GetComponent<Collider>().enabled = false;
+        
 
-    
+        GetComponent<Collider>().isTrigger = true;
         transform.parent = InputHandler.Instance.heldObjectPlaceHolder;
         transform.rotation = InputHandler.Instance.camera.transform.rotation;
         transform.position = transform.parent.position;
-
-
         rb.constraints = RigidbodyConstraints.FreezeAll;
-        rb.IsSleeping();
-
-
-        if (mirroredObject != null)
-            mirroredObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-
-
-        
     }
 
-    private void OnTriggerEnter(Collider collider) {
-        if (collider.tag == "Wall")
+    private void OnTriggerEnter(Collider collider)
+    {
+         if(collider.tag == "Wall")
         {
             Debug.Log("You hit a wall bitch");
+            GetComponent<Collider>().isTrigger = false;
             OnDrop();
-            
-            
         }
-        
+
+
+        else if (collider.tag == "Scanner")
+        {
+            Scanner scanner = collider.GetComponent<Scanner>();
+            if(scannerID == scanner.scannerCheckID)
+            {
+                scanner.OpenDoors();
+            }
+        }
     }
 
+    private void OnTriggerExit(Collider collider) 
+    {
+        if (collider.tag == "Scanner")
+        {
+            Scanner scanner = collider.GetComponent<Scanner>();
+            if(scannerID == scanner.scannerCheckID)
+            {
+                scanner.CloseDoors();
+            }
+        }
+    }
 }
+
