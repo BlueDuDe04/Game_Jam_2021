273cb3747c013a6ef7375886d1a05e28814c5307
ohmyzosh <ohmyzoshfilms@hotmail.com>
2021-06-13 00:56:00 -0500

    InputHandler: fixed Reticle - FixedUpdate, etc

--- Collaborative_PuzzleGame/Assets/Scripts/InputHandler.cs
+++ Collaborative_PuzzleGame/Assets/Scripts/InputHandler.cs
@@ -23,10 +23,10 @@ public class InputHandler : MonoBehaviour
             Instance = this; 
     }
 
-    private void Update() 
+    /*private void Update() 
     {
         isHoveringInteractable();
-        
+
         if (Input.GetKeyDown("e"))
         {
             if (isHoldingItem)
@@ -40,6 +40,26 @@ public class InputHandler : MonoBehaviour
                 Interact();
             }
             
+        }
+    }*/
+
+    private void FixedUpdate()
+    {
+        isHoveringInteractable();
+
+        if (Input.GetKeyDown("e"))
+        {
+            if (isHoldingItem)
+            {
+                isHoldingItem = false;
+                heldItem.OnDrop();
+                return;
+            }
+            else if (isHoveringInteractable())
+            {
+                Interact();
+            }
+
         }
     }
 
@@ -64,22 +84,28 @@ public class InputHandler : MonoBehaviour
 
     private bool isHoveringInteractable()
     {
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
         RaycastHit hit;
         Debug.DrawLine(ray.origin, ray.direction * 100f, Color.red);
 
         if (Physics.Raycast(ray, out hit)
         && isPlayerInRange(hit))
         {
-            if (hit.transform.tag == "Interactable")
+            if (hit.transform.CompareTag("Interactable"))
             {
                 hoveredItem = hit.transform.gameObject;
                 reticle.GetComponent<Image>().color = Color.green;
                 return true;
             }
-            reticle.GetComponent<Image>().color = Color.red;
-            return false;
+            else
+            {
+                reticle.GetComponent<Image>().color = Color.red;
+                return false;
+            }
         }
+        else
+            reticle.GetComponent<Image>().color = Color.red;
+
         hoveredItem = null;
         return false;
     }
